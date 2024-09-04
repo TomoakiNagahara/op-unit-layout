7989d0eaed4558dc94cbda2261ece53a64577bfb
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2024-09-04 19:21:24 +0900

    New: ci > Layout.php

---
+++ ci/Layout.php
@@ -0,0 +1,38 @@
+<?php
+/** op-unit-layout:/ci/Layout.php
+ *
+ * @created    2024-09-04
+ * @version    1.0
+ * @package    op-unit-layout
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/** Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP;
+
+/* @var $ci \OP\UNIT\CI\CI_Config */
+$ci = OP::Unit('CI')::Config();
+
+//	Template
+$path   = dirname(__DIR__)."/template/ci.txt";
+$method = 'Template';
+$args   = ['ci.txt'];
+$result = "Notice: This file is not located in the template directory. ({$path})";
+$ci->Set($method, $result, $args);
+
+//	Name
+$method = 'Name';
+$args   =  null;
+$result =  OP()->Config('Layout')['name'] ?? null;
+$ci->Set($method, $result, $args);
+
+//	...
+return $ci->Get();
