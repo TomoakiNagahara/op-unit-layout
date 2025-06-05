4e7c1e5f63cf695670a564b6ad4b5b3a857ff736
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-05 15:58:56 +0900

    Fix: ci > Layout.php | foreach( glob('Layout/*.php') as $path ){ include($path) }

--- ci/Layout.php
+++ ci/Layout.php
@@ -1,5 +1,5 @@
 <?php
-/** op-unit-layout:/ci/Layout.php
+/**	op-unit-layout:/ci/Layout.php
  *
  * @created    2024-09-04
  * @version    1.0
@@ -8,12 +8,7 @@
  * @copyright  Tomoaki Nagahara All right reserved.
  */
 
-/** Declare strict
- *
- */
-declare(strict_types=1);
-
-/** namespace
+/**	namespace
  *
  */
 namespace OP;
@@ -21,18 +16,10 @@ namespace OP;
 /* @var $ci \OP\UNIT\CI\CI_Config */
 $ci = OP::Unit('CI')::Config();
 
-//	Template
-$path   = OP()->MetaPath('asset:/unit/layout/template/ci.txt');
-$method = 'Template';
-$args   = ['ci.txt'];
-$result = "Notice: This file is not located in the template directory. ({$path})";
-$ci->Set($method, $result, $args);
-
-//	Name
-$method = 'Name';
-$args   =  null;
-$result =  OP()->Config('Layout')['name'] ?? null;
-$ci->Set($method, $result, $args);
+//	...
+foreach( glob('Layout/*.php') as $file_path ){
+	require_once($file_path);
+}
 
 //	...
 return $ci->Get();
